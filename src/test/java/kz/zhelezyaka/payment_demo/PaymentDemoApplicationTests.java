04f118186c7f93bf53ae0deb0d856de9110f9682
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2025-04-21 08:16:58 +0300

    Start project

---
+++ src/test/java/kz/zhelezyaka/payment_demo/PaymentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.payment_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
